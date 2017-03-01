bada9113d71bf0ceca4d7affeb52dde3907a7124
Florian Motlik <flomotlik@gmail.com>
2017-03-01 14:26:30 +0100

    Use sync for deployment to S3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,5 @@ hugo
 
 s3_path="s3://flomotlik.me"
 
-aws s3 rm $s3_path --recursive
-aws s3 cp public $s3_path --recursive --acl public-read
+# aws s3 rm $s3_path --recursive
+aws s3 sync public $s3_path --acl public-read --delete
